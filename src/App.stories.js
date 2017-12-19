83de160104035717efe8a145835fd9146a3ad256
Mauricio Coelho <mauscoelho@gmail.com>
2017-12-19 01:34:22 -0200

    :hammer: setup storyshots for tests

---
+++ src/App.stories.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import App from './App';
+
+storiesOf('App', module).add('with default', () => <App />);
--- storybook/storybook.js
+++ storybook/storybook.js
@@ -1,25 +1,13 @@
-/* eslint-disable global-require */
-import React, { Component } from 'react';
+/* eslint-disable import/no-extraneous-dependencies, 
+import/no-unresolved, import/extensions, global-require */
+
 import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
 
-// import stories
 configure(() => {
-  require('./stories');
+  require('./configureStories');
 }, module);
 
-// This assumes that storybook is running on the same host as your RN packager,
-// to set manually use, e.g. host: 'localhost' option
-const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true });
-
-// react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
-// https://github.com/storybooks/storybook/issues/2081
-// eslint-disable-next-line react/prefer-stateless-function
-class StorybookUIHMRRoot extends Component {
-  render() {
-    return <StorybookUIRoot />;
-  }
-}
-
-AppRegistry.registerComponent('bakeryApp', () => StorybookUIHMRRoot);
-export default StorybookUIHMRRoot;
+const StorybookUI = getStorybookUI({ port: 9001, onDeviceUI: true });
+AppRegistry.registerComponent('ArticlesApp', () => StorybookUI);
+export default StorybookUI;
