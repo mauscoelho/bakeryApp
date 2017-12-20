e472befbf7f0468f05625d3d01038c5ed0f7e777
Mauricio Coelho <mauscoelho@gmail.com>
2017-12-20 01:30:19 -0200

    :construction: show two cards in app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { StyleSheet, View } from 'react-native';
+import CustomerCard from './components/CustomerCard/index';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
 const App = () => (
   <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-    <Text style={styles.instructions}>To get started, edit App.js</Text>
-    <Text style={styles.instructions}>{instructions}</Text>
+    <CustomerCard
+      name={'Mauricio'}
+      lastName={'Coelho'}
+      address={'7889 West Tunnel Apt. 676 South Sherman, AK 16549'}
+      latestVisit={'1h'}
+    />
+    <CustomerCard
+      name={'João'}
+      lastName={'Silva'}
+      address={'Lombaardenstraat'}
+      latestVisit={'2h'}
+    />
   </View>
 );
 
--- src/App.stories.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import React from 'react';
-import { storiesOf } from '@storybook/react-native';
-import App from './App';
-
-storiesOf('App', module)
-  .add('with default', () => <App />)
-  .add('with default two', () => <App />);
