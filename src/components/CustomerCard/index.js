928845831257a8f2b129494e9e3b7a288b31a1e3
Mauricio Coelho <mauscoelho@gmail.com>
2017-12-20 00:40:43 -0200

    :art: add native-base structure for card

--- src/components/CustomerCard/index.js
+++ src/components/CustomerCard/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { PropTypes } from 'prop-types';
+import { Card, CardItem, Left, Body, Text, Right } from 'native-base';
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 0,
+  },
+});
 
 const CustomerCard = ({
   name, lastName, address, latestVisit,
 }) => (
-  <View>
-    <Text>{`${name} ${lastName}`}</Text>
-    <Text>{address}</Text>
-    <Text>{latestVisit}</Text>
-  </View>
+  <Card style={styles.card}>
+    <CardItem>
+      <Left>
+        <Text>{'img'}</Text>
+        <Body>
+          <Text>{`${name} ${lastName}`}</Text>
+          <Text note>{address}</Text>
+        </Body>
+      </Left>
+      <Right>
+        <Text>{latestVisit}</Text>
+      </Right>
+    </CardItem>
+  </Card>
 );
 
 CustomerCard.propTypes = {
